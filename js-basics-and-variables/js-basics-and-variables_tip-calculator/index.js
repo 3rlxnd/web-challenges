2280565fe27cf2663ce2948758872cb510c42678
3rlxnd <erlnd.gdt@gmail.com>
2024-12-15 00:36:12 +0100

    Solved

--- js-basics-and-variables/js-basics-and-variables_tip-calculator/index.js
+++ js-basics-and-variables/js-basics-and-variables_tip-calculator/index.js
@@ -14,3 +14,47 @@ based on the cost of the meal and the percentage of the tip.
 
 4. Log a message to the console that includes the meal cost, tip amount, and total cost.
 */
+
+
+
+// Method
+
+const totalCost = ((mealCost, tip) => {
+   let tipAmount = mealCost * tip / 100;
+   let totalCost = tipAmount + mealCost;
+
+   return parseFloat(totalCost.toFixed(2));
+});
+
+// Function
+
+function totalCostFunction(mealCost, tip) {
+   let tipAmount = mealCost * tip / 100;
+   let totalCost = tipAmount + mealCost;
+
+   return parseFloat(totalCost.toFixed(2));
+};
+
+// Menu
+
+class Meal {
+   constructor(name, price) {
+      this.name = name;
+      this.price = price;
+      this.tip = price * tipPercent / 100;
+      this.total = totalCost(price, tipPercent);
+   }
+}
+
+let menu = [];
+let tipPercent = 10;
+
+menu.push(new Meal("Soup", 8.90));
+menu.push(new Meal("Pasta", 15.90));
+menu.push(new Meal("Lamb", 19.90));
+menu.push(new Meal("Ragout", 13.90));
+menu.push(new Meal("Teramisu", 5.90));
+menu.push(new Meal("Red Wine", 7.90));
+menu.push(new Meal("Water", 3.50));
+
+console.table(menu)
