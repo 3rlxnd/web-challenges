d133794d33c23e665b5c370e8519a5f2da60407f
3rlxnd <erlnd.gdt@gmail.com>
2024-12-17 10:08:23 +0100

    Part 2 Solved

--- js-conditions-and-booleans/js-conditions-and-booleans_weather-check/js/index.js
+++ js-conditions-and-booleans/js-conditions-and-booleans_weather-check/js/index.js
@@ -1,24 +1,28 @@
 // Change this value to test different "weather" conditions.
-const weather = "rainy"
+const weather = "sunny"
 
 // Adjust the "temperature" value to trigger different messages (for Part 2 only).
-const temperature = 22; 
+const temperature = 21; 
 
 switch (weather) {
-    case "snowy" || temperature <= 0:
-        console.log("Freezing Snow!")
-        break;
+    case "sunny":
+      if (temperature > 20) {
+        console.log("It's sunny and warm!");
+      } else {
+        console.log("It's sunny, but a bit chilly!");
+      }
+      break;
     case "rainy":
-        console.log("It's raining, don't forget your umbrella!")
-        break;
-    case "sunny" || temperature <= 20:
-        temperature >= 20 ? console.log("It's sunny, but a bit chilly!") : console.log("It's sunny")
-        break;
-    case "sunny" || temperature >= 20:
-        temperature >= 20 ? console.log("It's sunny and warm!") : console.log("It's sunny")
-        break;
+      console.log("It's raining, don't forget your umbrella!");
+      break;
+    case "snowy":
+      if (temperature < 0) {
+        console.log("Freezing snow!");
+      } else {
+        console.log("It's snowing, stay warm!");
+      }
+      break;
     default:
-        console.log("I'm not sure what the weather is like.")
-        break;
-}
+      console.log("I'm not sure what the weather is like.");
+  }
 
