0ae95e6e0c421ed93513d3b8dfce2749c92e8a31
3rlxnd <erlnd.gdt@gmail.com>
2024-12-18 20:56:48 +0100

    Solve 'Change Box Appearance' Challenge

--- js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,21 @@
 console.clear();
+
+let box = document.querySelector('[data-js="box"]')
+let colorInput = document.querySelector('[data-js="input-color"]')
+let borderRadiusInput = document.querySelector('[data-js="input-radius"]')
+let rotationInput = document.querySelector('[data-js="input-rotation"]')
+
+colorInput.addEventListener("input", () => {
+    let color = colorInput.value;
+    box.style.backgroundColor = `hsl(${color}, 80%, 30%)`  
+})
+
+borderRadiusInput.addEventListener("input", () => {
+    let radius = borderRadiusInput.value;
+    box.style.borderRadius = `${radius}px`  
+})
+
+rotationInput.addEventListener("input", () => {
+    let rotation = rotationInput.value;
+    box.style.transform = `rotate(${rotation}deg)`  
+})
