b60abdfe11a615d14272a74ed1103d4357c03e02
3rlxnd <erlnd.gdt@gmail.com>
2025-01-09 11:45:31 +0100

    Solved 'Fetch' Challenge

--- js-error/js-error-handling_fetch/js/index.js
+++ js-error/js-error-handling_fetch/js/index.js
@@ -7,12 +7,23 @@ const errorElement = document.querySelector("[data-js='error']");
 async function fetchUserData(url) {
   try {
     const response = await fetch(url);
+    
+
+    // Check if the HTTP status is not OK
+    if (!response.ok && response.status == 404) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    
+    if (response.ok) {
+      throw new Error(`Invalid JSON! Status: ${response.status}`);
+    }
 
     return await response.json();
   } catch (error) {
     return { error: error.message };
   }
 }
+
 
 const endpoints = [
   { name: "User 1", url: "https://reqres.in/api/users/1" },
@@ -29,15 +40,21 @@ endpoints.forEach((endpoint) => {
   button.addEventListener("click", async () => {
     try {
       let result = await fetchUserData(endpoint.url);
+  
+      if (result.error) {
+        throw new Error(result.error);
+      }
+  
       const user = result.data;
       userElement.innerHTML = `
-      <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}" class="user__image"/>
-      <h2>${user.first_name} ${user.last_name}</h2>
+        <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}" class="user__image"/>
+        <h2>${user.first_name} ${user.last_name}</h2>
       `;
       errorElement.textContent = "";
     } catch (error) {
-      errorElement.textContent = error;
+      errorElement.textContent = `Error: ${error.message}`;
       userElement.innerHTML = "No user data available.";
     }
-  })
+  });
+  
 });
