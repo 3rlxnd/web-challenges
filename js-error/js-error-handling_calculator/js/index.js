5fc69151dbb561563861f22427b4d5a739d0e305
3rlxnd <erlnd.gdt@gmail.com>
2025-01-09 13:30:40 +0100

    Solved 'Calculator' Challenge

--- js-error/js-error-handling_calculator/js/index.js
+++ js-error/js-error-handling_calculator/js/index.js
@@ -9,18 +9,21 @@ const operations = {
   subtract: (a, b) => a - b,
   multiply: (a, b) => a * b,
   divide: (a, b) => {
-    try {
-      b == 0 ? error : a / b
-    } catch (error) {
-      errorMessage.innerHTML = "Cannot divide by zero!"
-    }
-  },
-};
+    if (b === 0) {
+      throw new Error("Can't divide by 0")
+    } else return a / b
+  }
+}
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const firstNumber = Number(event.target.firstNumber.value);
   const secondNumber = Number(event.target.secondNumber.value);
   const operation = event.target.operation.value;
-  output.innerText = operations[operation](firstNumber, secondNumber);
+  try {
+    output.innerText = operations[operation](firstNumber, secondNumber);
+    errorMessage.textContent = error.message;
+  } catch (error) {
+    errorMessage.textContent = error.message;
+  }
 });
