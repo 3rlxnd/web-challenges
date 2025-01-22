22d13d045c59c43d66ab6e79a2f44b1be4f96b9e
3rlxnd <erlnd.gdt@gmail.com>
2025-01-22 13:29:42 +0100

    Solved 'Stored Notes' Challenge

--- react-local-storage/react-with-local-storage_stored-note/src/App.jsx
+++ react-local-storage/react-with-local-storage_stored-note/src/App.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 import FontSelector from "./components/FontSelector";
 import Note from "./components/Note";
 import "./styles.css";
 
 export default function App() {
-  const [note, setNote] = useState("");
-  const [font, setFont] = useState("system-ui");
+  const [note, setNote] = useState(localStorage.getItem('notes') || "");
+  const [font, setFont] = useState(localStorage.getItem('font') || "system-ui");
 
   function handleNoteChange(newNote) {
     setNote(newNote);
+    localStorage.setItem('notes', newNote)
   }
 
   function handleFontChange(newFont) {
     setFont(newFont);
+    localStorage.setItem('font', newFont)
   }
 
   return (
