55d5b88b1ea36057543076ac5530921fca6b2fa6
3rlxnd <erlnd.gdt@gmail.com>
2025-01-29 17:44:55 +0100

    Solved 'Lights' Challenge

---
+++ react-global-state/react-global-state_lights/lib/lights.js
@@ -0,0 +1,12 @@
+export const lights = [
+    { name: 'Living Room', id: 1, isOn: false },
+    { name: 'Bedroom', id: 2, isOn: false },
+    { name: 'Kitchen', id: 3, isOn: false },
+    { name: 'Bathroom', id: 4, isOn: false },
+    { name: 'Hallway', id: 5, isOn: false },
+    { name: 'Garage', id: 6, isOn: false },
+    { name: 'Porch', id: 7, isOn: false },
+    { name: 'Office', id: 8, isOn: false },
+    { name: 'Basement', id: 9, isOn: false }
+  ];
+  
