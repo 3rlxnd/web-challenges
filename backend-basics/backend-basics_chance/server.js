555c3be33114ce061461d2c97d2cf745a74c4b32
3rlxnd <erlnd.gdt@gmail.com>
2025-02-10 11:22:45 +0100

    Solved 'Chance' Challenge

--- backend-basics/backend-basics_chance/server.js
+++ backend-basics/backend-basics_chance/server.js
@@ -1,3 +1,11 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+let chance = new Chance()
+
+export const server = createServer((req, res) => {
+    let name = chance.name()
+    let age = chance.age()
+    let profession = chance.profession()
+    res.end(`Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`)
+})
